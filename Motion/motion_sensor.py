a2acc282ab3f59244bde84bd4f81d6b2938f47b9
Madhav <madhav.humagain@ekbana.com>
2019-06-21 13:38:38 +0545

    Send motion data to IoT server in POST requst

--- Motion/motion_sensor.py
+++ Motion/motion_sensor.py
@@ -1,4 +1,8 @@
 import RPi.GPIO as GPIO
+import time
+import requests
+
+API_ENDPOINT = "10.10.10.172:1880/dat"
 
 #Note:
 #While using the function GPIO.setup(PIR_input, GPIO.IN) or GPIO.input(PIR_input), 
@@ -12,10 +16,32 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIR_input, GPIO.IN)
 
+HUMAN_PRESENT = 1
+HUMAN_NOT_PRESENT = 0
+
+motion = 0;
+last_state = HUMAN_NOT_PRESENT;
+
+last_saved = time.time();
+last_sent = time.time();
+
+
 while True:
-    if (GPIO.input(PIR_input)):
+    motion = GPIO.input(PIR_input)
+    if (motion):
         #HIGH means a change is detected in IR level
-        print("Motion detected.")
+        last_state = HUMAN_PRESENT
+        last_saved = time.time()
+        # print("Motion detected.")
     else:
         #LOW means there is no change in IR level
-        print("No motion.")
+        if (time.time()-last_saved > 30):
+            last_state = HUMAN_NOT_PRESENT
+            last_saved = time.time()
+
+    if (time.time() - last_sent > 30):
+        post_data = "{\"HUMAN_PRESENT\":" + str(last_state) + "}"
+        print ("Sending....\n",data)
+        res = requests.post(url=API_ENDPOINT, data=post_data)
+        print (res)
+        last_sent = time.time();
