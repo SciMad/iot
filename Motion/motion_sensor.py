06ecf4e96e7f9f4568af3c60406e366f45a95648
Madhav <madhav.humagain@ekbana.com>
2019-06-21 14:24:31 +0545

    Wait 10 sec for first POST request

--- Motion/motion_sensor.py
+++ Motion/motion_sensor.py
@@ -22,8 +22,8 @@ HUMAN_NOT_PRESENT = 0
 motion = 0;
 last_state = HUMAN_NOT_PRESENT;
 
-last_saved = time.time()-30;
-last_sent = time.time()-30;
+last_saved = time.time()-20;
+last_sent = time.time()-20;
 
 
 while True:
