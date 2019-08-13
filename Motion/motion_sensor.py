798136c6b89d25f99b357ad4377b47bc9de17771
Madhav <madhav.humagain@ekbana.info>
2019-08-13 04:56:42 -0400

    Change variable name to motion_in last5s

--- Motion/motion_sensor.py
+++ Motion/motion_sensor.py
@@ -21,7 +21,7 @@ HUMAN_PRESENT = 1
 HUMAN_NOT_PRESENT = 0
 
 motion = 0;
-motion_last5s = 0;
+motion_in_last5s = 0;
 last_state = HUMAN_NOT_PRESENT;
 
 last_seen = time.time()-5;
@@ -33,21 +33,21 @@ while True:
     if (motion):
         #HIGH means a change is detected in IR level
         last_state = HUMAN_PRESENT
-        motion_last5s = motion;
+        motion_in_last5s = motion;
         last_seen = time.time()
         # print("Motion detected.")
     else:
         #LOW means there is no change in IR level
 	if (time.time()-last_seen > 5):
-            motion_last5s = 0
+            motion_in_last5s = 0
         if (time.time()-last_seen > 35):
             last_state = HUMAN_NOT_PRESENT
-            #last_saved = time.time()
+            #last_seen = time.time()
 
     if (time.time() - last_sent > 2.5):	#send data at every 2.5 seconds
         last_sent = time.time();
         interval = int(last_sent - last_seen)
-        post_data = json.dumps({"LOCATION":"BATHROOM2", "HUMAN_PRESENT":last_state, "HUMAN_MOTION":motion,"MOTION_LAST_5S":motion_last5s, "LAST_ACTIVITY":str(interval)+" seconds ago"})
+        post_data = json.dumps({"LOCATION":"BATHROOM2", "HUMAN_PRESENT":last_state, "HUMAN_MOTION":motion,"MOTION_IN_LAST_5S":motion_in_last5s, "LAST_ACTIVITY":str(interval)+" seconds ago"})
         #post_data = '{\\'+'\"HUMAN_PRESENT'+ '\\'+  '\":' + str(last_state) + '}'
         print ("Sending....",post_data)
         try:
